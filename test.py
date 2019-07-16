5cbd2674db506fec5cb09e5bfdebe12b85ecaf87
13691579846 <281754043@qq.com>
2019-07-16 15:54:13 +0800

    html report dir and html report

--- test.py
+++ test.py
@@ -1,11 +1,14 @@
 """
 ------------------------------------
-@Time : 2019/7/11 10:21
+@Time : 2019/7/16 9:00
 @Auth : linux超
-@File : __init__.py.py
+@File : test.py
 @IDE  : PyCharm
 @Motto: Real warriors,dare to face the bleak warning,dare to face the incisive error!
 @QQ   : 28174043@qq.com
 @GROUP: 878565760
 ------------------------------------
 """
+from selenium import webdriver
+
+driver = webdriver.Firefox()
