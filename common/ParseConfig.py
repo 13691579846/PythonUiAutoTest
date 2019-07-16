67c97fcbce13d4e153d99a1b8a2514464e485cd5
13691579846 <281754043@qq.com>
2019-07-16 15:51:16 +0800

    new public function

--- common/ParseConfig.py
+++ common/ParseConfig.py
@@ -9,3 +9,36 @@
 @GROUP: 878565760
 ------------------------------------
 """
+from configparser import (ConfigParser,
+                          NoOptionError,
+                          NoSectionError)
+
+
+class ParseConfig(ConfigParser):
+
+    def __init__(self, filename):
+        super().__init__()
+        self.filename = filename
+        self.read(self.filename, encoding='utf-8')
+
+    def get_option_value(self, section, option=None):
+        """get value of section with option"""
+        try:
+            if option is None:
+                value = dict(self[section])
+                return value
+            value = self.get(section, option)
+            if '->' in value:
+                value = tuple(value.split("->"))
+            return value
+        except (NoOptionError, NoSectionError) as e:
+            raise e
+
+    def __call__(self, *args, **kwargs):
+        return self.get_option_value(*args, **kwargs)
+
+
+if __name__ == '__main__':
+    from config.config import CONFIG_PATH
+    config = ParseConfig(CONFIG_PATH)
+    print(config('LoginPage', 'phone_input'))
