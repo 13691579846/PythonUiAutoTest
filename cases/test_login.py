063634463562a4f47c75cce080b31e2eb4136e33
13691579846 <281754043@qq.com>
2019-07-16 15:50:22 +0800

    new test case function

--- cases/test_login.py
+++ cases/test_login.py
@@ -9,3 +9,51 @@
 @GROUP: 878565760
 ------------------------------------
 """
+import unittest
+from selenium import webdriver
+
+from pages.LoginPage import LoginPage
+from datas.LoginDatas import LoginData
+from libs.ddt import ddt, data
+
+
+@ddt
+class TestLogin(unittest.TestCase):
+    """登录测试用例"""
+    @classmethod
+    def setUpClass(cls):
+        cls.driver = webdriver.Firefox()
+        cls.driver.maximize_window()
+        cls.login = LoginPage(cls.driver)
+
+    def setUp(self):
+        self.login.open_url()
+
+    @data(*LoginData.login_success_data)
+    def test_login_success(self, value):
+        self.login.login(value['user'], value['pwd'])
+        actual = self.login.get_login_success_info()
+        self.assertEqual(value['expect'], actual, msg='断言失败')
+
+    @data(*LoginData.login_format_data)
+    def test_login_format_error(self, value):
+        self.login.login(value['user'], value['pwd'])
+        actual = self.login.get_phone_pwd_format_info()
+        self.assertEqual(value['expect'], actual, msg='断言失败')
+
+    @data(*LoginData.login_account_error_data)
+    def test_login_account_error(self, value):
+        self.login.login(value['user'], value['pwd'])
+        actual = self.login.get_phone_pwd_error_info()
+        self.assertEqual(value['expect'], actual, msg='断言失败')
+
+    def tearDown(self):
+        self.driver.delete_all_cookies()
+
+    @classmethod
+    def tearDownClass(cls):
+        cls.driver.quit()
+
+
+if __name__ == '__main__':
+    unittest.main()
